32c5e8ddbf5984b0d44cf12e7399a46ed6781962
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-11 17:00:44 -0300

    added authentication function to google button and redirect to newRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
+import { useHistory } from "react-router-dom";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
+  //para fazer o redirecionamento para outra página
+  const history = useHistory();
+
+  //importando o meu contexto
+  const { user, singInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    //se o usuário não estiver autenticado quando clicar no botão de entrar com o google, será chamado o método singInWithGoogle
+    if (!user) {
+      await singInWithGoogle();
+    }
+
+    //para redirecionar o usuário para a tela de criação de uma sala.
+    history.push("/rooms/new");
+  }
+
   return (
     <div id="page-auth">
       {/* Parte lateral esquerda roxa com as informações apenas */}
@@ -22,7 +40,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="logo do google" />
             Crie sua sala com o Google
           </button>

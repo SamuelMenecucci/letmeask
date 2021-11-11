937b45746d8d1ce90ed09c150f94941bcbf7595b
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-11 08:54:29 -0300

    stylization with scss

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      {/* Parte lateral esquerda roxa com as informações apenas */}
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="ilustração simbolizando perguntas e respostas "
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      {/* parte  lateral direita com os inputs e opções de entrada*/}
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+
+          <form action="">
+            <input type="text" placeholder="Nome da sala " />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente ? <a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}

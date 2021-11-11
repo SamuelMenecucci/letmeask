5445020cc2c7cbb3736b3e1e9b019cf43a768ede
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-11 16:58:47 -0300

    created routers and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 export function App() {
-  return <NewRoom />;
+  return (
+    //TODO Ver como fazer o redirecionamento com a nova versão do react-router-dom. essa maneira é com a versão 5.2.0 e a última lançada é a 6.0.1
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/rooms/new" component={NewRoom}></Route>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }

393c67fcfbffe102884a31fe37a397699543864b
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-12 13:42:50 -0300

    room page updated and styles

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,47 @@
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+
+//função do router para pegar os parametros da rota
+import { useParams } from "react-router-dom";
+import "../styles/room.scss";
+
+//tipagem do parametro que irei pegar
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
-  return <h1>sala</h1>;
+  //aonde guardo o parametro que peguei da rota da minha página. como estou passano o id da sala no parametro, irei pegar esse id com o useParams. ele está pegando o parametro com a key id pois é o parametro que passamos na rota, lá no app, utilizando o route. Ele trás a rota dentro de um objeto
+  const params = useParams<RoomParams>();
+
+  console.log(params);
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={params.id} />
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar ?" />
+
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }

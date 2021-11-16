7f2167b3cc894005a5bd18d68095bda87bd3be78
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-15 22:02:13 -0300

    created hook to pick up questions that already exists in database. moved from Room to useRoom

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { database } from "../services/firebase";
+
+//para declarar a tipagem de um objeto no typescript, utilizo o Record.a primeira posição é a chave e a segunda é o value
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHithlighted: boolean;
+  }
+>;
+
+//tipagem para o estado que irá armazenar a pergunta
+type QuestionType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
+
+//criação de um hook para a reutilização de suas funcionalidades em outras partes da aplicação.
+export function useRoom(roomId: string) {
+  //o estado será um array de questions
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+
+  const [title, setTitle] = useState("");
+
+  //o useEffect é uma função (hook) que dispara um evento sempre que alguma informação mudar. mas se eu passo o array de dependencia como vazio, essa função vai executar uma única vez assim que o componente for exibido em tela.
+  //utilizo o useEffect para ir dentro do firebase e buscar as perguntas já existentes.
+  useEffect(() => {
+    //buscando a referencia para a minha sala dentro do firebase
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    //buscar os dados das perguntas.
+    //o once irá buscar os dados apenas uma vez, o on irá fazer mais de uma. como primeiro parametro
+    ///doc https://firebase.google.com/docs/database/web/read-and-write#web_value_events
+    //doc sobre parametros possiveis para serem ouvidos. https://firebase.google.com/docs/database/admin/retrieve-data#section-event-types
+    roomRef.on("value", (room) => {
+      //
+      const databaseRoom = room.val();
+
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      //o retorno que eu tenho do .once é um objeto, mas eu preciso de um array para poder fazer a iteração e a exibição, por isso utilizo o Objetc.entries(), que irá que retornar uma matriz com a key/value em cada posição.
+      //DOC: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        //como utilizei o entries, eu tenho duas posições. faço a desestruturação aqui para conseguir fazer a iteração da forma que eu preciso.
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHithlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  return { questions, title };
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -10,36 +10,11 @@ import { FormEvent, useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 import { Question } from "../components/Question";
+import { useRoom } from "../hooks/useRoom";
 
 //tipagem do parametro que irei pegar
 type RoomParams = {
   id: string;
-};
-
-//para declarar a tipagem de um objeto no typescript, utilizo o Record.a primeira posição é a chave e a segunda é o value
-type FirebaseQuestions = Record<
-  string,
-  {
-    author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isAnswered: boolean;
-    isHithlighted: boolean;
-  }
->;
-
-//tipagem para o estado que irá armazenar a pergunta
-type QuestionType = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
 };
 
 export function Room() {
@@ -53,45 +28,8 @@ export function Room() {
 
   const [newQuestion, setNewQuestion] = useState("");
 
-  //o estado será um array de questions
-  const [questions, setQuestions] = useState<QuestionType[]>([]);
-
-  const [title, setTitle] = useState("");
-
-  //o useEffect é uma função (hook) que dispara um evento sempre que alguma informação mudar. mas se eu passo o array de dependencia como vazio, essa função vai executar uma única vez assim que o componente for exibido em tela.
-  //utilizo o useEffect para ir dentro do firebase e buscar as perguntas já existentes.
-  useEffect(() => {
-    //buscando a referencia para a minha sala dentro do firebase
-    const roomRef = database.ref(`rooms/${roomId}`);
-
-    //buscar os dados das perguntas.
-    //o once irá buscar os dados apenas uma vez, o on irá fazer mais de uma. como primeiro parametro
-    ///doc https://firebase.google.com/docs/database/web/read-and-write#web_value_events
-    //doc sobre parametros possiveis para serem ouvidos. https://firebase.google.com/docs/database/admin/retrieve-data#section-event-types
-    roomRef.on("value", (room) => {
-      //
-      const databaseRoom = room.val();
-
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-
-      //o retorno que eu tenho do .once é um objeto, mas eu preciso de um array para poder fazer a iteração e a exibição, por isso utilizo o Objetc.entries(), que irá que retornar uma matriz com a key/value em cada posição.
-      //DOC: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-      const parsedQuestions = Object.entries(firebaseQuestions).map(
-        //como utilizei o entries, eu tenho duas posições. faço a desestruturação aqui para conseguir fazer a iteração da forma que eu preciso.
-        ([key, value]) => {
-          return {
-            id: key,
-            content: value.content,
-            author: value.author,
-            isHighlighted: value.isHithlighted,
-            isAnswered: value.isAnswered,
-          };
-        }
-      );
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions);
-    });
-  }, [roomId]);
+  //informações de dentro do hook que criei
+  const { questions, title } = useRoom(roomId);
 
   async function handleSendNewQuestion(event: FormEvent) {
     event.preventDefault();

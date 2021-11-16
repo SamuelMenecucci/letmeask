d87a5c7435976181acd2bb31be50d2fbe265dabe
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-16 09:41:21 -0300

    finished question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import "../styles/question.scss";
 
+import cn from "classnames"; //yarn add classnames
+
 type QuestionProps = {
   content: string;
   author: {
@@ -9,12 +11,31 @@ type QuestionProps = {
   };
   //ao passar o reactnode na tipagem do children, podemos passar qualquer conteudo jsx para o children, html, div, algum componente, button e etc.
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
 //estou criando a pergunta em um componente pois a exibição das perguntas irá se repetir várias vezes, então, podemos fazer o reaproveitamento de código
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  children,
+  isAnswered,
+  isHighlighted,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      // pacote classnames que instalo com o yarn para facilitar a escrita das classes que possuem alguma condição.
+      className={cn(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+
+      // className={`question ${isAnswered ? "answered" : ""} ${
+      //   isHighlighted ? "highlighted" : ""
+      // }  }`}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">

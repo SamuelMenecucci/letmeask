ea2805af02c6ed992d954394449b605b80c0c45a
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-15 23:56:53 -0300

    like functionality in questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,7 +6,7 @@ import toast from "react-hot-toast";
 //função do router para pegar os parametros da rota
 import { useParams } from "react-router-dom";
 import "../styles/room.scss";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 import { Question } from "../components/Question";
@@ -60,6 +60,24 @@ export function Room() {
     setNewQuestion("");
   }
 
+  //para fazer a funcionalidade de like da pergunta
+  async function handleLikeQuestion(
+    questionId: string,
+    likeId: string | undefined
+  ) {
+    if (likeId) {
+      //se o usuário já tiver dado like, ele remove o likes
+      await database
+        .ref(`rooms/${roomId}/questions/${questionId}/likes/${likeId}`)
+        .remove();
+    } else {
+      //adicionando o like e passando o caminho dentro do firebase da pergunta
+      await database
+        .ref(`rooms/${roomId}/questions/${questionId}/likes`)
+        .push({ authorId: user?.id }); //.push irá colocar os dados do id do autor
+    }
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -111,13 +129,19 @@ export function Room() {
                 author={question.author}
               >
                 <button
-                  className="like-button"
+                  className={`like-button ${question.likeId ? "liked" : ""}`}
                   type="button"
                   arial-label="marcar como gostei"
+                  //quando vou chamar uma função no onClick que vá receber algum parametro, preciso passar sempre com a arrow function dessa forma. se eu apensar chamar a função direta passando o parametro, eu estou passando a execução da função.
+                  onClick={() =>
+                    handleLikeQuestion(question.id, question.likeId)
+                  }
                 >
                   {/*  ps: para a exibição de qualquer conteudo dentro de um componente, é necessário declarar o children no arquivo do mesmo.  */}
 
-                  <span>10</span>
+                  {question.likeCount > 0 && (
+                    <span> {question.likeCount} </span>
+                  )}
 
                   {/* jogo diretamente o código svg do like dentro do html, pois dessa forma eu consigo manipular a cor dele, coisa que não seria possível se estivessemos utilizando uma tag img para a exibição do mesmo. a cor é a propriedade stroke.  */}
                   <svg
@@ -130,9 +154,9 @@ export function Room() {
                     <path
                       d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
                       stroke="#737380"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
                 </button>

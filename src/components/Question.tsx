b9f76c1d303cb522c8acedfc018a872873e7fa46
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-15 22:58:29 -0300

    added children

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import "../styles/question.scss";
 
 type QuestionProps = {
@@ -6,10 +7,12 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  //ao passar o reactnode na tipagem do children, podemos passar qualquer conteudo jsx para o children, html, div, algum componente, button e etc.
+  children?: ReactNode;
 };
 
 //estou criando a pergunta em um componente pois a exibição das perguntas irá se repetir várias vezes, então, podemos fazer o reaproveitamento de código
-export function Question({ content, author }: QuestionProps) {
+export function Question({ content, author, children }: QuestionProps) {
   return (
     <div className="question">
       <p>{content}</p>
@@ -18,7 +21,7 @@ export function Question({ content, author }: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   );
